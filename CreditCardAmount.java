ecfd5980a0f3412874da6e7076bc657e1b14961b
RojaSiraparapu <siraparapuroja2003@gmail.com>
2025-05-23 19:35:35 +0530

    Add files via upload

---
+++ CreditCardAmount.java
@@ -0,0 +1,63 @@
+
+package com.encapsulation;
+public class CreditCardAmount {
+	private String customerName;
+	private double creditLimit;
+	private double currentDebt;
+	private double interestRate;
+	public CreditCardAmount(String customerName,double creditLimit,double currentDebt,double interestRate) {
+		this.customerName=customerName;
+		this.creditLimit=creditLimit;
+		this.currentDebt=currentDebt;
+		this.interestRate=interestRate;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public double getCreditLimit() {
+		return creditLimit;
+	}
+	public void setCreditLimit(double creditLimit) {
+		this.creditLimit = creditLimit;
+	}
+	public double getCurrentDebt() {
+		return currentDebt;
+	}
+	public void setCurrentDebt(double currentDebt) {
+		this.currentDebt = currentDebt;
+	}
+	public double getInterestRate() {
+		return interestRate;
+	}
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	public void swipeCard(double amount) {
+		if(amount>this.creditLimit) {
+			System.out.println("Transcation declined:Limit exceeded");
+		}else {
+			System.out.println("increase currentDebt");
+		}
+	}
+	public void makePayment(double amount) {
+		if(amount>this.currentDebt) {
+			System.out.println("Excess payment not allowed");
+		}
+	}
+	public void applyInterest() {
+		this.currentDebt+=(this.currentDebt*(this.interestRate/100.0));
+		System.out.println(this.currentDebt);
+	}
+
+	public static void main(String[] args) {
+		CreditCardAmount amt=new CreditCardAmount("Roja",100000,2500,2);
+		amt.swipeCard(75000);
+		amt.makePayment(55000);
+		amt.applyInterest();
+	}
+}
+
+
